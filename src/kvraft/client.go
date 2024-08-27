8747b60bacefe2a1078adcff5d58d499d28f89b2
yinn_x <yinnnxt@gmail.com>
2024-08-27 20:32:40 +0800

    feat: naive raft client

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,20 +1,16 @@
 package kvraft
 
-import "6.5840/labrpc"
-import "crypto/rand"
-import "math/big"
+import (
+	"sync/atomic"
 
+	"github.com/google/uuid"
+
+	"6.5840/labrpc"
+)
 
 type Clerk struct {
-	servers []*labrpc.ClientEnd
-	// You will have to modify this struct.
-}
-
-func nrand() int64 {
-	max := big.NewInt(int64(1) << 62)
-	bigx, _ := rand.Int(rand.Reader, max)
-	x := bigx.Int64()
-	return x
+	servers     []*labrpc.ClientEnd
+	cacheLeader uint64
 }
 
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
@@ -22,6 +18,28 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck.servers = servers
 	// You'll have to add code here.
 	return ck
+}
+
+func (ck *Clerk) TryCall(svcMeth string, args interface{}) interface{} {
+	i := atomic.LoadUint64(&ck.cacheLeader)
+	DPrintf("call: %s %+v", svcMeth, args)
+	opID := uuid.New().String()
+	for {
+		var res Response
+		i = i % uint64(len(ck.servers))
+		if ck.servers[i].Call(svcMeth, &Request{
+			OpID: opID,
+			Args: args,
+		}, &res) {
+			err := res.Err
+			if err == OK {
+				atomic.StoreUint64(&ck.cacheLeader, i)
+				return res.Reply
+			}
+			DPrintf("call failed: %s", err)
+		}
+		i++
+	}
 }
 
 // fetch the current value for a key.
@@ -35,9 +53,10 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
-
-	// You will have to modify this function.
-	return ""
+	args := GetArgs{Key: key}
+	reply := ck.TryCall("KVServer.Get", &args)
+	DPrintf("ok: Get %+v - %+v", args, reply)
+	return reply.(GetReply).Value
 }
 
 // shared by Put and Append.
@@ -49,7 +68,12 @@ func (ck *Clerk) Get(key string) string {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+	}
+	ck.TryCall("KVServer."+op, &args)
+	DPrintf("ok: %s %+v - %+v", op, args, nil)
 }
 
 func (ck *Clerk) Put(key string, value string) {
